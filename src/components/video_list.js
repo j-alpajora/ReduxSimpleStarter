8bed7f781e5624582e98e6d597f671cc65f5e68d
Jeza Alpajora <jeza.alpajora@finder.com.au>
2016-08-11 08:42:35 +0800

    REACT: Bonus session

--- src/components/video_list.js
+++ src/components/video_list.js
@@ -2,10 +2,18 @@ import React from 'react';
 import VideoListItem from './video_list_item';
 
 const VideoList = (props) => {
+  const videoItems = props.videos.map((video) => {
+    return (
+      <VideoListItem
+        onVideoSelect={props.onVideoSelect}
+        key={video.etag}
+        video={video} />
+    );
+  });
 
   return (
     <ol className="col-md-4 list-group">
-      {props.videos.length}
+      {videoItems}
     </ol>
   );
 };
